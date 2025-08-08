c89e6b90dc2df3ff0b1a326afcb3355bf31e02fd
llyfn <tyv001008@gmail.com>
2025-08-08 11:21:49 +0900

    feat: implement dataset split

--- src/dataset.py
+++ src/dataset.py
@@ -1,15 +1,12 @@
 import torch
 import pandas as pd
 from torch.utils.data import Dataset
-from typing import Dict, Union
 from transformers import AutoTokenizer
+from sklearn.model_selection import train_test_split
 
 class SarcasmDataset(Dataset):
-    def __init__(self, data_source: Union[str, pd.DataFrame], model_name, max_length: int):
-        if isinstance(data_source, str):
-            self.data = pd.read_csv(data_source)
-        else:
-            self.data = data_source
+    def __init__(self, data_source, model_name, max_length: int):
+        self.data = pd.read_csv(data_source)
         self.data = self.data.dropna(subset=['comment']).reset_index(drop=True)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
         self.max_length = max_length
@@ -20,7 +17,7 @@ class SarcasmDataset(Dataset):
     def __len__(self) -> int:
         return len(self.comments)
 
-    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+    def __getitem__(self, idx):
         comment = str(self.comments[idx])
         parent_comment = str(self.parent_comments[idx])
         label = int(self.labels[idx])
@@ -42,3 +39,14 @@ class SarcasmDataset(Dataset):
             'labels': torch.tensor(label, dtype=torch.long),
             'text': input_text
         }
+
+def load_dataset(data_source, model_name, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1, max_length=128, random_state=42):
+    data = pd.read_csv(data_source)
+    train_val_data, test_data = train_test_split(data, test_size=test_ratio, random_state=42, stratify=data['label'])
+    train_data, val_data = train_test_split(train_val_data, test_size=val_ratio/(train_ratio + val_ratio), random_state=random_state, stratify=train_val_data['label'])
+
+    train_dataset = SarcasmDataset(train_data, model_name, max_length)
+    val_dataset = SarcasmDataset(val_data, model_name, max_length)
+    test_dataset = SarcasmDataset(test_data, model_name, max_length)
+
+    return train_dataset, val_dataset, test_dataset
