1833faf0dbfab897cf2aad8da3f43985782ad8da
llyfn <tyv001008@gmail.com>
2025-11-11 16:40:02 +0900

    feat: use mean pool for pretraining

--- src/loss.py
+++ src/loss.py
@@ -6,17 +6,19 @@ class ContrastiveLoss(nn.Module):
     def __init__(self, temperature=0.07):
         super(ContrastiveLoss, self).__init__()
         self.temperature = temperature
+        self.eps = 1e-9
 
     def forward(self, hidden, labels):
         hidden = F.normalize(hidden, dim=-1)
-        similarity_matrix = torch.matmul(hidden, hidden.T) / self.temperature
 
-        positive_mask = (labels.unsqueeze(1) == labels.unsqueeze(0)).float()
-        positive_mask.fill_diagonal_(0)
+        labels_matrix = labels.unsqueeze(0) == labels.unsqueeze(1)
+        logits_mask = torch.ones_like(labels_matrix, dtype=torch.bool).fill_diagonal_(False)
+        positive_mask = labels_matrix & logits_mask
 
-        exp_sim = torch.exp(similarity_matrix)
-        pos_sum = (exp_sim * positive_mask).sum(1) / (positive_mask.sum(1) + 1e-9)  # positives 평균 sim
-        total_sum = exp_sim.sum(1)
+        sim = torch.matmul(hidden, hidden.T) / self.temperature
+        sim_no_diag = sim.masked_fill(torch.eye(hidden.shape[0], dtype=torch.bool, device=hidden.device), -self.eps)
+        log_prob = sim_no_diag - torch.logsumexp(sim_no_diag, dim=1, keepdim=True)
+        mean_log_prob_pos = (positive_mask * log_prob).sum(dim=1) / (positive_mask.sum(dim=1) + self.eps)
+        loss = -mean_log_prob_pos.mean()
 
-        loss = -torch.log(pos_sum / total_sum).mean()
         return loss
--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from transformers import AutoModel
 
@@ -8,12 +9,20 @@ class SarcasmModel(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.classifier = nn.Linear(self.model.config.hidden_size, num_labels)
 
+    def mean_pool(self, last_hidden_state, attention_mask):
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
+        sum_embed = torch.sum(last_hidden_state * input_mask_expanded, 1)
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        return sum_embed / sum_mask
+
     def forward(self, input_ids, attention_mask, is_pretraining=False):
         output = self.model(input_ids=input_ids, attention_mask=attention_mask)
-        hidden = output.last_hidden_state[:, 0, :]
 
-        if is_pretraining: return hidden
+        if is_pretraining:
+            hidden = self.mean_pool(output.last_hidden_state, attention_mask)
+            return hidden
         else:
+            hidden = output.last_hidden_state[:, 0, :]
             pooled_output = self.dropout(hidden)
             logits = self.classifier(pooled_output)
             return logits
